9b994df365daeceaf3fab158addc05db836fedd4
Systems2025 <systems@blueskykenya.org>
2025-05-18 20:38:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     // --- CONFIGURATION ---
-    const ZAPIER_CHECK_AVAILABILITY_WEBHOOK_URL = 'lytlfwirjs60pb2kljoq88ltm21x7c3f@hook.eu2.make.com'; // <-- REPLACE
+    const ZAPIER_CHECK_AVAILABILITY_WEBHOOK_URL = 'https://hook.eu2.make.com/g4ps2ybsu0561400xn9gzgj79k1vuwdg'; // <-- REPLACE
     const ZAPIER_SUBMIT_BOOKING_WEBHOOK_URL = 'https://hooks.zapier.com/hooks/catch/20644786/27k2lwr/';     // <-- REPLACE
 
     // DOM Elements
